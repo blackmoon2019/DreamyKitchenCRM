9e62fefc634d382b0b5be8d26d6216a0f790dc39
John Mavroselinos <johnmavroselinos@gmail.com>
2024-09-05 18:32:39 +0300

    V3.8.1.3

--- DreamyKitchenCRM/My Project/AssemblyInfo.vb
+++ DreamyKitchenCRM/My Project/AssemblyInfo.vb
@@ -32,6 +32,6 @@ Imports System.Runtime.InteropServices
 ' <Assembly: AssemblyVersion("1.0.*")>
 
 '<<<<<<< Updated upstream
-<Assembly: AssemblyVersion("3.8.1.2")>
-<Assembly: AssemblyFileVersion("3.8.1.2")>
+<Assembly: AssemblyVersion("3.8.1.3")>
+<Assembly: AssemblyFileVersion("3.8.1.3")>
 
